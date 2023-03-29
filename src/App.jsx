10cae87fd106f8621b4ca41c6c9e0b25f77f6c6c
Harsh Vyapari <harshv1741@gmail.com>
2023-03-29 19:29:35 +0530

    feat(Vite, React): Initializing and Structuring App

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="Title">
+        <h1>🎵 Media Player 🎵</h1>
+        <h3>This media player is designed using React + Vite Environment</h3>
+      </section>
+
+      <section className="StatusBar">
+        <h1 id="StatusScreen"></h1>
+      </section>
+
+      <section className="MediaControls"></section>
+    </div>
+  );
+}
+
+export default App;
